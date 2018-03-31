30b2963649fb2841d4c0121c10678694b0c1c206
Iryna Zhdanova <zhdanovairyna@gmail.com>
2018-03-31 23:19:05 +0300

    style: Format code
    Add missing spaces, semi-colons, redundant spaces

--- js/app.js
+++ js/app.js
@@ -1,7 +1,22 @@
 /*
  * Create a list that holds all of your cards
  */
-const iconSet = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-anchor', 'fa fa-leaf', 'fa fa-bicycle', 'fa fa-diamond', 'fa fa-bomb', 'fa fa-leaf', 'fa fa-bomb', 'fa fa-bolt', 'fa fa-bicycle', 'fa fa-paper-plane-o', 'fa fa-cube'];
+const iconSet = ['fa fa-diamond', 
+'fa fa-paper-plane-o',
+'fa fa-anchor',
+'fa fa-bolt',
+'fa fa-cube',
+'fa fa-anchor',
+'fa fa-leaf',
+'fa fa-bicycle',
+'fa fa-diamond',
+'fa fa-bomb',
+'fa fa-leaf',
+'fa fa-bomb',
+'fa fa-bolt',
+'fa fa-bicycle',
+'fa fa-paper-plane-o',
+'fa fa-cube'];
 
 // Declare variables
 const deck = document.querySelector('.deck');
@@ -10,9 +25,9 @@ const cards = document.querySelectorAll('.deck li');
 
 const restart = document.querySelector('.restart');
 
-let openedCards = [];
+let openedCards = []; // to store opened cards
 
-let matchedCards = [];
+let matchedCards = []; // to store match cards
 
 let moveCount = document.querySelector('.moves');
 
@@ -20,7 +35,7 @@ let moves = 0;// move counter (two opened cards)
 
 let clicks = 0; // click counter
 
-const starList = document.querySelector('.stars'); 
+const starList = document.querySelector('.stars');
 
 let stars =  document.querySelectorAll('.stars li');
 
@@ -44,7 +59,6 @@ const closeModalButton = document.querySelector('.close-modal');
 
 const playAgainButton = document.querySelector('.play-again');
 
-
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -61,7 +75,7 @@ restart.addEventListener('click', function() {
         return false;
     } else {
         startGame();
-    } 
+    }
 });
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -89,29 +103,33 @@ function startGame() {
     clicks = 0;
     moves = 0;
     moveCount.innerHTML = moves;
+
     // reset star rating
     for (let i = 0; i < stars.length; i++) {
         stars[i].firstElementChild.className = 'fa fa-star';
     }
+
     //reset timer
     seconds = 0;
     minutes = 0;
     hours = 0;
     stopTimer();
     timer.innerHTML = addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds);
+
     //empty the list of cards
     deck.innerHTML = '';
 
     //shuffle the icons
     let shuffledIconSet = shuffle(iconSet);
     
-    //add a card from cards nodeList with only class 'card' 
-    cards.forEach(function(card){
+    //add a card from cards nodeList with only class 'card'
+    cards.forEach(function(card) {
         deck.appendChild(card);
         card.classList = 'card';
+
         //add an icon to each card
         for (let i = 0; i < shuffledIconSet.length; i++) {
-        cards[i].firstElementChild.className = shuffledIconSet[i];
+            cards[i].firstElementChild.className = shuffledIconSet[i];
         }
     });
 }
@@ -130,15 +148,16 @@ function startGame() {
 // Function to open cards 
 deck.addEventListener('click', function(event) {
     //Check if user clicked a card on the deck
-    if(!(event.target.className === 'deck') && (openedCards.length <= 2)) {
+    if (!(event.target.className === 'deck') && (openedCards.length <= 2)) {
         openCard(event);
         addToOpenedCards(event);
-    } 
+    }
 });
 
 //A function to open a card's icon
 function openCard(event) {
-    if(openedCards.length < 2) {
+    // allow card opening only if there is less than two cards opened
+    if (openedCards.length < 2) {
         event.target.className = 'card open show disable';
     } else {
         return false;
@@ -148,16 +167,17 @@ function openCard(event) {
 //Add opened card to the list of opened cards
 function addToOpenedCards(event) {
     openedCards.push(event.target.firstElementChild);
+    // Count moves
     moveCounter();
     // Check if two cards match or not happens when there're two opened cards
-    if (openedCards.length === 2) {        
-        // Compare if two opened cards match  
+    if (openedCards.length === 2) {
+        // Compare if two opened cards match
         if (openedCards[0].classList.value === openedCards[1].classList.value) {
             match(openedCards, matchedCards);
         } else {
             notMatch(openedCards);
         }
-    } 
+    }
 }
 
 // Function to run if two opened cards do match
@@ -171,9 +191,10 @@ function match(arr1, arr2) {
         arr2.push(arr1[0], arr1[1]);
         emptyArray(arr1);
     }, 800);
+
     // when all cards do match matched cards list contains 16 items and modal appears
-    setTimeout(function(){
-        if(arr2.length === 16) {
+    setTimeout(function() {
+        if (arr2.length === 16) {
             gameWon();
         }
     }, 1000);
@@ -187,7 +208,7 @@ function emptyArray(arr) {
 // Function to run if two opened cards do not match
 // Cards get closed and become available for clicks again
 function notMatch(arr) {
-    setTimeout(function(){
+    setTimeout(function() {
         arr[0].parentNode.classList.add('mismatch');
         arr[1].parentNode.classList.add('mismatch');
     }, 500);
@@ -195,7 +216,7 @@ function notMatch(arr) {
         arr[0].parentNode.classList.remove('open', 'show', 'disable', 'mismatch');
         arr[1].parentNode.classList.remove('open', 'show', 'disable', 'mismatch');
         openedCards = [];
-    }, 1500); 
+    }, 1500);
 }
 
 //Count clicks function
@@ -208,26 +229,28 @@ function moveCounter() {
     } else {
         moveCount.nextElementSibling.innerHTML = ' Moves';
     }
-    // Display move count 
+    
+    // Display move count
     moveCount.innerHTML = moves;
+    // Start timer on first click
     if (clicks === 1) {
-        time = setInterval(function(){
+        time = setInterval(function() {
             timeCount();
         }, 1000);
     }
 
-    // change stars rating according to the move count
+    // Change stars rating according to the move count
     if (moves >= 0 && moves < 13) {
         for (let i = 0; i < stars.length; i++) {
             stars[i].firstElementChild.className = 'fa fa-star';
         }
-        modalHeader.innerText = 'That Was Awesome!'
+        modalHeader.innerText = 'That Was Awesome!';
     } else if (moves >= 13 && moves <= 18) {
         starList.lastElementChild.firstElementChild.className = 'fa fa-star-o';
-        modalHeader.innerText = 'Great Job!'
+        modalHeader.innerText = 'Great Job!';
     } else if (moves > 18) {
         starList.lastElementChild.previousElementSibling.firstElementChild.className = 'fa fa-star-o';
-        modalHeader.innerText = 'Congratulations! You Won!'
+        modalHeader.innerText = 'Congratulations! You Won!';
     }
 }
 
@@ -246,12 +269,12 @@ function timeCount() {
     // Add zeros on timer
     timer.innerHTML = addZero(hours) + ':' + addZero(minutes) + ':' + addZero(seconds);
     seconds++;
-    if(seconds === 60) {
+    if (seconds === 60) {
         minutes++;
         seconds = 0;
     }
        
-    if(minutes === 60) {
+    if (minutes === 60) {
         hours++;
         minutes = 0;
     }
@@ -260,11 +283,12 @@ function timeCount() {
 // Stop timer
 function stopTimer() {
     clearInterval(time);
+
     //Change pause button icon to 'play'
     pauseBtn.firstElementChild.className = 'fa fa-play-circle-o';
 }
 
-// Add zeros to timer displayed on the page 
+// Add zeros to timer displayed on the page
 function addZero(num) {
     if (num < 10) {
         return '0' + num;
@@ -277,10 +301,12 @@ function addZero(num) {
 function gameWon() {
     stopTimer();
     modal.style.display = 'block';
+
     // Display final scores in modal
     finalMoves.innerHTML = moveCount.innerHTML;
     finalTimer.innerHTML = timer.innerHTML;
     finalStars.innerHTML = 'Rating: ' + starList.innerHTML;
+
     //disable the pause button from working when modal is open
     pauseBtn.firstElementChild.className = 'fa fa-play-circle-o';
     pauseBtn.classList.add('disable');
@@ -296,7 +322,7 @@ closeModalButton.addEventListener('click', function() {
     closeModal();
 });
 
-// Event listener for 'Play again!' button 
+// Event listener for 'Play again!' button
 //to close modal and start new game
 playAgainButton.addEventListener('click', function() {
     closeModal();
@@ -330,5 +356,3 @@ pauseBtn.addEventListener('click', function() {
         play();
     }
 });
-
-
